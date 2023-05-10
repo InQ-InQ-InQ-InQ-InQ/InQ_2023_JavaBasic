edd983e5e5e2f572a9e9e924e6dbc201ed387f51
Choi_Young_Woo <101418352+piedra-de-flor@users.noreply.github.com>
2023-05-10 21:02:07 +0900

    [3주차] 최영우 과제 제출 (#39)
    * [3주차] 최영우 과ã제 제출
    
    * fixed untracked files
    
    * fixed untracked files

---
+++ src/영우/Week3_2439.java
@@ -0,0 +1,20 @@
+package 영우;
+
+import java.util.*;
+
+public class Week3_2439 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = i; k > 0; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
